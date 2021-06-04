87e49ec08cdd6fb0f6a7230082f541aebe68735a
VTorres09 <vt.macedo@hotmail.com>
2021-06-04 16:28:04 -0300

    Commenting js

--- templates/static/js/chords.js
+++ templates/static/js/chords.js
@@ -1,4 +1,6 @@
 class Chord {
+
+  //construtor da classe
   constructor(beat, start, chord, x, y, i) {
     this.beat = beat;
     this.start = start;
@@ -15,16 +17,14 @@ class Chord {
     this.index = i;
   }
 
+  //Função chamada quando o usuario clica na tela
   clicked(px, py) {
     if(px > this.x-5 && px < this.x-5 + 10 && py>this.y-36 && py<this.y-36+this.rh){
-      //locked = true;
       this.selected = true;
-      console.log("boa")
     } else {
-      //locked = false;
       this.selected = false;
     }
-    let s = this.x+(window.wid/50)+(amp/2)
+    let s = this.x+(window.wid/50)
     if(px >= s&&px < s+100 && py >= this.y-this.rh&&py < this.y){
       this.replace = true;
     }else{
@@ -33,12 +33,9 @@ class Chord {
 
   }
 
+  //Função para acompanhar o mouse quando o usuário alterar o tempo do acorde
   moving(px, limit_left = 0, limit_right){
     if(this.selected && px>limit_left && px<limit_right){
-        //console.log(this.start)
-        //console.log(px)
-        //console.log(this.x)
-        //console.log(this.start)
         let offset = (px - this.x)
         this.x = this.x + offset;
         this.fixed_x = this.fixed_x + offset;
@@ -46,18 +43,13 @@ class Chord {
     }
   }
 
+  //Função que mapeia a posição do acorde de acordo com tempo da musica
   move() {
-      //console.log("BAR: ", bar_wid)
-      //console.log("BAR: ", song_duration)
-      //console.log("t0: ", t0)
-      //console.log("Curr: ", current_time)
-      //console.log("curr: ", currentTime())
       let position = map(current_time, 0, duration, 0, bar_wid);
-      //console.log("TIME: ", current_time)
       this.x = this.fixed_x - position;
-      //console.log(this.x)
   }
 
+  //Função usada para alterar o acorde quando o usuario requisitar
   change(){
     if(this.changing){
       let inp = createInput("novo acorde: ");
@@ -65,15 +57,15 @@ class Chord {
     }
   }
 
+  //Função criada para renderizar os acordes
   show() {
     textAlign(LEFT)
     fill(this.brightness, 125);
-    text(this.chord, this.x+(window.wid/50)+(amp/2), this.y);
-    //textFont(fontRoboto);
+    text(this.chord, this.x+(window.wid/50), this.y);
     textSize(window.hei/12);
     stroke(0, 0, 0, 0)
     fill(0, 255, 255, 150);
-    rect(this.x-5, this.y-36, this.rw+(amp/2), this.rh)
+    rect(this.x-5, this.y-36, this.rw, this.rh)
     textSize(window.hei/20);
     text(parseFloat(this.start).toFixed(2), (this.x-window.wid/50), this.y+(this.y/5));
     if(this.x <= window.wid/2+10 && this.x >= window.wid/2-10){
--- templates/static/js/sketch.js
+++ templates/static/js/sketch.js
@@ -1,71 +1,87 @@
-let song_mp3;
-let song_json;
-let x_inicial;
+//Variavéis globais não inicializadas
+let song_json, duration, c_input, bar_wid;
+
+//Variavéis globais pré-definidas
 let locked = false;
-let bx;
 let pause = true;
 let editing = false;
-let amplitude;
-let amp = 0;
 let animations = false;
 let current_time = 0;
 let last_current_time = 0;
-let duration;
-let c_input;
 let t = -1;
 let chord_now = [0, '']
-let song_duration, bar_wid;
+let index;
 
 
-function preload(){
-  //soundFormats('mp3');
-  //song_mp3 = createAudio('assets/teste1.mp3');
-  song_json = result
-  //fontRoboto = loadFont('assets/Roboto-Black.ttf');
+//A função preload carrega qualquer arquivo necessário antes de rodar qualquer outra função do p5
+function preload() {
+
+    //Atribuimos o JSON da música obtido na "p5page.html" a uma variável global do p5
+    song_json = result
+
 }
 
+//Definimos um array de acordes
 var chords = [];
 
+//A função setup, nativa do p5, é executada uma única vez após o termino da função preload
 function setup() {
-  window.wid = 1000;
-  window.hei = 600;
-  createCanvas(window.wid, window.hei).parent('canvasHolder');
-  amplitude = new p5.Amplitude();
-  parseChords(chords);
-  bar_wid = chords[chords.length-1].x
 
-  c_input = createInput()
-  c_input.position((window.wid/2)+(c_input.width/1.2)*0.6, (window.hei/2)+(c_input.height)*1.2)
-  c_button = createButton('update');
-  c_button.position((c_input.x + c_input.width), (window.hei/2)+(c_input.height)*1.2);
-  c_button.mousePressed(update_chord);
-  c_input.color = (0, 0, 255)
+    //Definimos variáveis globais para o tamanho da tela usando o window.
+    window.wid = 1000;
+    window.hei = 600;
+
+    //Criamos o canvas onde o p5 irá renderizar os gráficos
+    createCanvas(window.wid, window.hei).parent('canvasHolder');
+
+    //A função parseChords irá preencher o array de acordes criado previamente com os acordes do JSON
+    parseChords(chords);
+
+    //Acessamos o último acorde do array para definir qual será o comprimento da barra de acordes
+    bar_wid = chords[chords.length - 1].x
+
+    //Criamos a area de input para a substituição de acordes
+    c_input = createInput()
+    c_input.position(((window.wid / 2) + (c_input.width / 1.2)), (window.hei / 2) + (c_input.height) * 1.2)
+    c_button = createButton('✎ update');
+    c_button.position(c_input.x + c_input.width, (window.hei / 2) + (c_input.height) * 1.2);
+    c_button.mousePressed(update_chord);
+    c_input.color = (0, 255, 255)
+    //Vale lembrar que essa area de input ainda não é criada pelo p5, e por isso não é dinâmica como os outros elementos da página
 }
 
-function parseChords(chords){
-  for (var i in song_json) {
-    var beat = song_json[i]["current_beat"];
-    var start = song_json[i]["current_beat_time"];
-    var chord = song_json[i]["estimated_chord"];
-    var x = findNextX(i, start);
-    var y = window.hei/3;
-    var a = new Chord(beat, start, chord, x, y, i);
-    chords.push(a);
-  }
+//A função parseChords, chamada no setup(), vai percorrer o JSON e popular o array com os acordes encontrados
+function parseChords(chords) {
+
+    //Percorremos o JSON
+    for (var i in song_json) {
+
+        //adquirimos o beat, o tempo(start) e o acorde em si do acorde atual no JSON
+        var beat = song_json[i]["current_beat"];
+        var start = song_json[i]["current_beat_time"];
+        var chord = song_json[i]["estimated_chord"];
+
+        //findNextX irá definir a posição no eixo X de cada acorde
+        var x = findNextX(i, start);
+        var y = window.hei / 3;
+
+        //Criamos um objeto do tipo Chord, descrito em "chord.js", e inserimos no array
+        var a = new Chord(beat, start, chord, x, y, i);
+        chords.push(a);
+    }
 }
 
-function findNextX(i, start){
-  if (i > 0){
-    return start*100;
-  }else{
-    return song_json[i]["current_beat_time"];
-  }
+//A função finNextX, chamada em parseChords irá definir a posição X de cada acorde
+function findNextX(i, start) {
+    if (i > 0) {
+        return start * 100;
+    } else {
+        return song_json[i]["current_beat_time"];
+    }
 }
 
+//A função draw, nativa do p5, irá atualizar a tela repetidamente
 function draw() {
-
-    //Como a função draw atualiza repetidamente, salvamos o valor passado do tempo atual, que será necessário mais pra frente
-
 
     //Aqui utilizamos a função ontimeupdate, que atualiza independentemente da função draw, e retorna o timestamp da música
     audio.ontimeupdate = function () {
@@ -79,7 +95,7 @@ function draw() {
     /*
     Como a função ontimeupdate atualiza a mais ou menos 4fps e a função draw atualiza a 60fps, para evitar lags e
     animações cortadas, suavizamos a incrementação do current_time enquanto a próxima atualização do ontimeupdate
-    não é disponibilizada
+    não é disponibilizada.
     */
     if (!pause && current_time != last_current_time) {
 
@@ -90,185 +106,161 @@ function draw() {
         let indice = window.wid/65000
         current_time += indice
     }
-  if(!pause && animations){
-    if(amp==25 || amp==0){
-      t = -1*t
-      amp = amp + t
-    }else{
-      amp = amp + t
-    }
-  }
 
-    rect(window.wid/2, window.hei/8, 50, 50)
+    rect(window.wid / 2, window.hei / 8, 50, 50)
     rect(window.wid / 2 - 40, window.hei / 3, 40, 80)
-    if (animations) {
-      //amp = 25 * amplitude.getLevel();
-      text("Desativar animação", 0, 0)
-    } else {
-      //amp = 0;
-      text("Ativar animação", 0, 0)
-    }
     background(0, 0, 0);
-    playButton(amp);
+
+    //A função playButton renderiza os botões e outros elementos na tela
+    playButton();
+
+    //A função drawRects renderiza as caixas em que os acordes ficam contidos, baseado na posição desses
     drawRects();
+
+    //Percorremos todos os acordes
     for (var i in chords) {
-      if (!pause) {
-        chords[i].move();
-      }
-      chords[i].show();
-      chords[i].change();
+        if (!pause) {
+            //Enquanto a musica está sendo reporduzida os acordes movem em relação ao tempo da música
+            chords[i].move();
+        }
+        //Aqui os acordes sao renderizados
+        chords[i].show();
+        chords[i].change();
     }
-    //}
-  //}
-  //chords[i].move();
+
+
 }
 
-function update_chord(){
-    //if(editable){
-      console.log("CURRENT: ", chord_now[1])
-      chord_now[1] = c_input.value()
-      console.log("NEW CURRENT: ", chord_now[1])
-      chords[chord_now[0]].chord = c_input.value()
-      console.log("DEBUG: ", chords[chord_now[0]].chord, "\n", "Index: ", chord_now[0])
-    //}
+//Nessa função obtemos o input do usuario para alterar o acorde
+function update_chord() {
+    chord_now[1] = c_input.value()
+    chords[chord_now[0]].chord = c_input.value()
 }
 
-function drawRects(){
-  let i = 0;
-  for(i in chords){
-    if(i>0){
-      let r = 20*((i%2)+1);
-      fill(30+r, 30+r, 30+r, 25+r)
-      rect(chords[i-1].x, chords[i].y-36, chords[i].x-chords[i-1].x, chords[i].rh);
+//drawRects renderiza os retangulos que os acordes estão inseridos, dinamicamente baseado na posição dos mesmos
+function drawRects() {
+    let i = 0;
+    for (i in chords) {
+        if (i > 0) {
+            let r = 20 * ((i % 2) + 1);
+            fill(30 + r, 30 + r, 30 + r, 25 + r)
+            rect(chords[i - 1].x, chords[i].y - 36, chords[i].x - chords[i - 1].x, chords[i].rh);
+        }
     }
-  }
 }
 
-
-function playButton(amp){
-    let radius = window.hei/13
-    let xc = window.wid/2
-    let yc = window.hei/1.6
+//PlayButton renderiza os botões e mais alguns elementos na tela
+function playButton() {
+    let radius = window.hei / 13
+    let xc = window.wid / 2
+    let yc = window.hei / 1.6
 
     fill(0, 255, 255)
     textAlign(CENTER)
-    text(chord_now[1], window.wid/2, window.hei/8)
+    text(chord_now[1], window.wid / 2, window.hei / 8)
 
     fill(0, 255, 255)
-    triangle(xc, window.hei/4, xc-radius, window.hei/5, xc+radius, window.hei/5)
+    triangle(xc, window.hei / 4, xc - radius, window.hei / 5, xc + radius, window.hei / 5)
 
-    if(pause){
-      txt = "Play"
-      fill(50, 50, 50)
-    }else{
-      circle(window.wid/2, window.hei/1.6, radius)
-      fill(0, 255, 255)
-      circle((window.wid/2)*1.2, window.hei/1.6, radius*0.7)
-      circle((window.wid/2)*0.8, window.hei/1.6, radius*0.7)
-      txt = "Pause"
+    if (pause) {
+        txt = "Play"
+        fill(50, 50, 50)
+    } else {
+        circle(window.wid / 2, window.hei / 1.6, radius)
+        fill(0, 255, 255)
+        circle((window.wid / 2) * 1.2, window.hei / 1.6, radius * 0.7)
+        circle((window.wid / 2) * 0.8, window.hei / 1.6, radius * 0.7)
+        txt = "Pause"
     }
-      circle((window.wid/2)*1.2, window.hei/1.6, radius*0.7)
-      circle((window.wid/2)*0.8, window.hei/1.6, radius*0.7)
-      rect((window.wid/2)*1.8, yc-(0.5*radius), radius*2, radius, 200)
-      circle(xc, yc, radius)
-      textAlign(CENTER)
-      textSize(window.hei/20)
-      fill(0, 255, 255)
-      text(txt, window.wid/2, window.hei/1.32)
+    circle((window.wid / 2) * 1.2, window.hei / 1.6, radius * 0.7)
+    circle((window.wid / 2) * 0.8, window.hei / 1.6, radius * 0.7)
+    rect((window.wid / 2) * 1.8, yc - (0.5 * radius), radius * 2, radius, 200)
+    circle(xc, yc, radius)
+    textAlign(CENTER)
+    textSize(window.hei / 20)
+    fill(0, 255, 255)
+    text(txt, window.wid / 2, window.hei / 1.32)
 
 
-    if(pause){
-      fill(0, 255, 255)
-      triangle(xc+(radius*0.7), yc, xc-(0.5*radius), yc-(0.5*radius), xc-(0.5*radius), yc+(0.5*radius))
-      textSize(window.hei/20)
-      text('>>', (window.wid/2)*1.2, window.hei/1.56)
-      text('<<', (window.wid/2)*0.8, window.hei/1.56)
-      textSize(window.hei/25)
-      text('Send', (window.wid/2)*1.89, (yc-(0.1*radius))*1.04)
-    }else{
-      fill(50, 50, 50)
-      rect(xc-(0.5*radius), yc-(0.5*radius), radius*0.3, radius)
-      rect(xc+(0.5*radius)-(radius*0.35), yc-(0.5*radius), radius*0.3, radius)
-      textSize(window.hei/20)
-      text('>>', (window.wid/2)*1.2, window.hei/1.56)
-      text('<<', (window.wid/2)*0.8, window.hei/1.56)
-      textSize(window.hei/25)
-      text('Send', (window.wid/2)*1.89, (yc-(0.1*radius))*1.04)
+    if (pause) {
+        fill(0, 255, 255)
+        triangle(xc + (radius * 0.7), yc, xc - (0.5 * radius), yc - (0.5 * radius), xc - (0.5 * radius), yc + (0.5 * radius))
+        textSize(window.hei / 20)
+        text('>>', (window.wid / 2) * 1.2, window.hei / 1.56)
+        text('<<', (window.wid / 2) * 0.8, window.hei / 1.56)
+        textSize(window.hei / 25)
+        text('Send', (window.wid / 2) * 1.89, (yc - (0.1 * radius)) * 1.04)
+    } else {
+        fill(50, 50, 50)
+        rect(xc - (0.5 * radius), yc - (0.5 * radius), radius * 0.3, radius)
+        rect(xc + (0.5 * radius) - (radius * 0.35), yc - (0.5 * radius), radius * 0.3, radius)
+        textSize(window.hei / 20)
+        text('>>', (window.wid / 2) * 1.2, window.hei / 1.56)
+        text('<<', (window.wid / 2) * 0.8, window.hei / 1.56)
+        textSize(window.hei / 25)
+        text('Send', (window.wid / 2) * 1.89, (yc - (0.1 * radius)) * 1.04)
     }
 }
 
+/*
+mousePressed é uma função nativa do p5 que ativa sempre que o mouse é pressionado.
+Usamos para identificar cliques nos botões usando a posição do mouse
+*/
 function mousePressed() {
-  for (var i in chords) {
-    chords[i].clicked(mouseX, mouseY);
-  }
-  let d = dist(mouseX, mouseY, window.wid/2, window.hei/1.6)
-  if(d < window.hei/15){
-    if(pause){
-      audio.play();
-      pause=false;
-    }else{
-      audio.pause();
-      pause=true
+    for (var i in chords) {
+        chords[i].clicked(mouseX, mouseY);
     }
-  }
-  let d2 = dist(mouseX, mouseY, (window.wid/2)*1.2, window.hei/1.6)
-  if(d2 < (window.hei/13)*0.7){
-      audio.currentTime += 5.0;
-
-  }
-
-  let d3 = dist(mouseX, mouseY, (window.wid/2)*0.8, window.hei/1.6)
-  if(d3 < (window.hei/13)*0.7){
-       audio.currentTime -= 5.0;
-  }
-
-  let d4 = dist(mouseX, mouseY, (window.wid/2)*1.95, window.hei/1.6)
-  if(d4 < (window.hei/11)){
-      //enviar array chords para o server e resolver problema da pausa
-    $.post( "/postmethod", {
-      canvas_data: JSON.stringify(chords)
-    }, function(err, req, resp){
-      //Redireciona para a pagina do resultado
-      window.location.href = "/result/";
-    });
-  }
-
-  let d5 = dist(window.wid/2, window.hei/8, mouseX, mouseY)
-  if(d5 < 50){
-    if(editing){
-      editing = false
-    }else{
-      editing = true
+    let d = dist(mouseX, mouseY, window.wid / 2, window.hei / 1.6)
+    if (d < window.hei / 15) {
+        if (pause) {
+            audio.play();
+            pause = false;
+        } else {
+            audio.pause();
+            pause = true
+        }
     }
-  }
+    let d2 = dist(mouseX, mouseY, (window.wid / 2) * 1.2, window.hei / 1.6)
+    if (d2 < (window.hei / 13) * 0.7) {
+        audio.currentTime += 5.0;
+
+    }
+
+    let d3 = dist(mouseX, mouseY, (window.wid / 2) * 0.8, window.hei / 1.6)
+    if (d3 < (window.hei / 13) * 0.7) {
+        audio.currentTime -= 5.0;
+    }
+
+    let d4 = dist(mouseX, mouseY, (window.wid / 2) * 1.95, window.hei / 1.6)
+    if (d4 < (window.hei / 11)) {
+
+        $.post("/postmethod", {
+            canvas_data: JSON.stringify(chords)
+        }, function (err, req, resp) {
+
+            window.location.href = "/result/";
+        });
+    }
+
+    let d5 = dist(window.wid / 2, window.hei / 8, mouseX, mouseY)
+    if (d5 < 50) {
+        if (editing) {
+            editing = false
+        } else {
+            editing = true
+        }
+    }
 }
 
+//Função nativa o p5 usada para identificar movimento de drag do mouse
 function mouseDragged() {
     for (var i in chords) {
-      if(i == 0){
-      chords[i].moving(mouseX, 0, 10000000);
-      }else if(i+1 == null){
-      chords[i].moving(mouseX, 0, 10000000);
-      }else{
-      chords[i].moving(mouseX, 0, 10000000);
-      }
+        if (i == 0) {
+            chords[i].moving(mouseX, 0, 10000000);
+        } else if (i + 1 == null) {
+            chords[i].moving(mouseX, 0, 10000000);
+        } else {
+            chords[i].moving(mouseX, 0, 10000000);
+        }
     }
 }
-
-function mouseReleased() {
-  //locked = false;
-  //console.log("soltou")
-}
-
-function lastChord(ex_json){
-  for(var key in ex_json){
-    if(ex_json.hasOwnProperty(key)){
-        lastKey = key;
-    }
-    return lastKey;
-  }
-}
-
-//function currentTime(){
-  //return (millis()-t0)/1000
-//}
